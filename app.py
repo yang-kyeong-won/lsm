ece02c15a18cc8b2eb30a50fb425e0829880e88e
yang-kyeong-won <lrqunax@gmail.com>
2024-02-04 00:47:53 +0900

    encoding

--- app.py
+++ app.py
@@ -9,20 +9,30 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.pushBt_file_chc.clicked.connect(self.pushBt_file_chc_clicked)
+        self.pushBt_diagnosis.clicked.connect(self.pushBt_diagnosis_clicked)
 
     def pushBt_file_chc_clicked(self):
         fname = QFileDialog.getOpenFileName(self, '파일 열기', '', 'All File(*)', 'All Files(*.*)')
         if fname[0]:
-            print("파일 경로")
-            print(fname[0])
+            print("파일 경로 : " + fname[0])
+            global df
             df = pd.read_csv(fname[0], encoding='cp949')
             print(df)
         else:
             print("파일 안 골랐음")
-
-
+    def pushBt_diagnosis_clicked(self):
+        # 열 이름 체크
+        isalpha = 0
+        for i in df.columns:
+            if i.encode().isalpha():
+                isalpha = isalpha + 1
+            else:
+                continue
+        if isalpha == len(df.columns):
+            print("열 이름 모두 영어")
+        else:
+            print("열 이름 한글 존재")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
